86c25f0d494a060be612cc6aae9ce3504744bed9
IvanVincentKwenandar <c14200138@john.petra.ac.id>
2022-06-09 17:08:34 +0700

    LAST PUSH

--- api/post_data_game3.php
+++ api/post_data_game3.php
@@ -6,7 +6,7 @@
     $timers = (int)$_POST['timers'];
     $score = (int)$_POST['score'];
     $id_team1 = (int)$_POST['idp1'];
-    $difficulties = $_POST['difficulties'];
+    $difficulties = (int)$_POST['difficulties'];
 
     $sql = "INSERT INTO `game3` (`id`, `time`, `score`, `id_team1`, `difficulties`) VALUES ('$id_game',$timers,$score,$id_team1,$difficulties)";
     $result = mysqli_query($con, $sql);
